5b0987803247a45d1a7708008ae04caadb040443
Yanguk <yanguk@duck.com>
2024-07-20 20:47:35 +0900

    nextjs init

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function BlogPage() {
+  return (
+    <div>BlogPage 준비중...</div>
+  )
+}
+
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <h1>준비중 입니다..</h1>
+      </div>
+    </main>
+  );
+}
