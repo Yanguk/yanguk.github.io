2df9fbd6c560535998e263e84520c90629ae4b7b
Yanguk <yanguk@duck.com>
2024-07-21 09:17:53 +0900

    Readme fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yanguk Full-stack Web Developer",
+  title: "Yanguk",
   description: "blog",
 };
 
