75c423200a2aa1936ba7dbdd70965f18846ade94
Yanguk <yanguk@duck.com>
2025-03-01 01:40:52 +0900

    다크모드 지원

--- SkunkHtml.fs
+++ SkunkHtml.fs
@@ -6,7 +6,7 @@
     let generateFinalHtml (head: string) (header: string) (footer: string) (content: string) (script: string) =
         $"""
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang="en" color-mode="user">
         <head>
             {head}
         </head>
