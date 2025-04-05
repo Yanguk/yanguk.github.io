b071eae0f8f69b446e2a9316e36fab52d2fa6b20
Yanguk <yanguk@duck.com>
2025-04-05 17:02:46 +0900

    feat: google search tag

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'yanguk Blog',
+  title: 'Yanguk Blog',
   author: 'yanguk',
   headerTitle: 'Yanguk',
   description: 'Web Developer Blog',
-  language: 'en-us',
+  language: 'ko-KR',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/Yanguk/yanguk.github.io',
