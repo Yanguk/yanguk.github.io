c360bfccf4356f772fff8c48dbf98feeef656c35
Yanguk <yanguk@duck.com>
2025-04-05 17:22:24 +0900

    feat: 부트캠프 개인 project 추가

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Visual-system',
+    description: `부트캠프 개인 프로젝트로 진행한 시스템 모니터링앱입니다.`,
+    imgSrc: '/static/images/visual-system.png',
+    href: 'https://github.com/Yanguk/Visual-system',
   },
 ]
 
