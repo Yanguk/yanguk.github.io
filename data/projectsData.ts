2841f33a9b387df9589c313c8f7a0dea636d0ab7
Yanguk <yanguk@duck.com>
2025-04-05 20:00:02 +0900

    feat: add project circle

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,9 +8,15 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Visual-system',
-    description: `부트캠프 개인 프로젝트로 진행한 시스템 모니터링앱입니다.`,
+    description: `Vanilla JS + electron + D3.js 로 구성된 시스템 모니터링앱 입니다.`,
     imgSrc: '/static/images/visual-system.png',
     href: 'https://github.com/Yanguk/Visual-system',
+  },
+  {
+    title: 'Circle',
+    description: `WebRTC와 Socket.io를 기반으로 구현된 실시간 음성 채팅 서비스입니다.`,
+    imgSrc: '/static/images/circle-project.png',
+    href: 'https://github.com/orgs/Team-TTT/repositories',
   },
 ]
 
